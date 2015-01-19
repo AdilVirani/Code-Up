b9822be5e9d34f18940b98b16c184adfd0007359
AdilVirani <adisv9997@gmail.com>
2015-01-19 01:56:04 -0600

    Round 1: Complete

---
+++ Code-Up/Code-Up/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Code-Up
+//
+//  Created by Adil  Virani on 1/19/15.
+//  Copyright (c) 2015 Adil  Virani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Code-Up/Code-Up/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Code-Up
+//
+//  Created by Adil  Virani on 1/19/15.
+//  Copyright (c) 2015 Adil  Virani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
